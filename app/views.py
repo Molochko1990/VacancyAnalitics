19dfcc2c0ad20e8026c1360cb8c2bb6d0b24488d
Molochko1990 <es01@bk.ru>
2025-01-07 12:04:32 +0500

    Небольшие изменения

--- app/views.py
+++ app/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def statistics_view(request):
+    # Здесь будет логика получения и обработки данных
+    context = {}  # Контекст для передачи данных в шаблон
+    return render(request, 'statistics.html', context)
--- project/urls.py
+++ project/urls.py
@@ -21,4 +21,5 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('statistics/', views.statistics_view, name='statistics'),
 ]
